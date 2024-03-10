bf21ef1e22dd0d9a27b3e7402c7cd654dcb33982
Ojelade Olamide <emmanuelojelade7@gmail.com>
2024-03-10 06:45:56 +0100

    update index file

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -53,6 +53,32 @@
             </div>
 
             <div class="mb-8">
+              <label for="gender" class="text-[1.6rem]">Gender</label>
+              <vee-field
+                as="select"
+                name="gender"
+                class="w-full outline-none border text-[2.2rem] border-b p-4 rounded-[1rem]"
+                v-model="userData.usergender"
+              >
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value=""></option>
+              </vee-field>
+              <ErrorMessage class="text-red-600" name="gender" />
+            </div>
+
+            <div class="mb-8">
+              <label for="crop_type" class="text-[1.6rem]">Crop Type</label>
+              <vee-field
+                type="text"
+                name="crop_type"
+                class="w-full outline-none border text-[2.2rem] border-b p-4 rounded-[1rem]"
+                v-model="userData.crop_type"
+              />
+              <ErrorMessage class="text-red-600" name="crop_type" />
+            </div>
+
+            <div class="mb-8">
               <label for="nin" class="text-[1.6rem]">National Identification Number - NIN</label>
               <vee-field
                 type="text"
@@ -61,6 +87,32 @@
                 v-model="userData.usernin"
               />
               <ErrorMessage class="text-red-600" name="nin" />
+            </div>
+
+            <div class="mb-8">
+              <label for="street_address" class="text-[1.6rem]">Address</label>
+              <vee-field
+                type="text"
+                name="street_address"
+                class="w-full outline-none border text-[2.2rem] border-b p-4 rounded-[1rem]"
+                v-model="userData.street_address"
+              />
+              <ErrorMessage class="text-red-600" name="street_address" />
+            </div>
+
+            <div class="mb-8">
+              <label for="lga" class="text-[1.6rem]">Local Government Area</label>
+              <vee-field
+                as="select"
+                name="lga"
+                class="w-full outline-none border text-[2.2rem] border-b p-4 rounded-[1rem]"
+                v-model="userData.userlga"
+              >
+                <option value="Abadam">Abadam</option>
+                <option value="Abaji">Abaji</option>
+                <option value=""></option>
+              </vee-field>
+              <ErrorMessage class="text-red-600" name="lga" />
             </div>
 
             <div class="mb-8">
@@ -110,21 +162,6 @@
                 <option value=""></option>
               </vee-field>
               <ErrorMessage class="text-red-600" name="state" />
-            </div>
-
-            <div class="mb-8">
-              <label for="gender" class="text-[1.6rem]">Gender</label>
-              <vee-field
-                as="select"
-                name="gender"
-                class="w-full outline-none border text-[2.2rem] border-b p-4 rounded-[1rem]"
-                v-model="userData.usergender"
-              >
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-                <option value=""></option>
-              </vee-field>
-              <ErrorMessage class="text-red-600" name="gender" />
             </div>
 
             <div class="mb-8">
@@ -189,6 +226,9 @@ export default {
         phone: 'required|min:11|max:11|numeric',
         nin: 'required|min:11|max:11|numeric',
         state: 'required',
+        crop_type: 'required|min:3|max:11',
+        street_address: 'required|min:3|max:100',
+        lga: 'required',
         gender: 'required|min:4|max:6',
         country: 'required',
         password: 'required|min:9|max:100|excluded:password'
@@ -205,6 +245,9 @@ export default {
       phone: '',
       usernin: '',
       userstate: '',
+      street_address: '',
+      crop_type: '',
+      lga: '',
       usercountry: '',
       usergender: '',
       userlanguage: '',
@@ -222,13 +265,14 @@ export default {
         },
         nin: userData.value.usernin,
         bvn: '12933390303',
-        crop_type: 'maize',
+        crop_type: userData.value.crop_type,
         gender: userData.value.usergender,
-        street_address: '12, PO BOX, Angeles Avenue',
+        street_address: userData.value.street_address,
         state: userData.value.userstate,
-        lga: 'Alimosho Local Govt',
+        lga: userData.value.userlga,
         country: userData.value.usercountry
       }
+      console.log(data)
       registerUser(data)
     }
 
